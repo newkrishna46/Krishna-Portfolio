5332d645c4236e155692a788bed0dd64512b2291
newkrishna46 <chenchukrishna46@gmail.com>
2026-08-14 18:52:49 +0530

    Update contact form

--- frotend/script.js
+++ frotend/script.js
@@ -1,48 +1,18 @@
-
-const contactForm = document.getElementById("contactForm");
-const successMessage = document.getElementById("successMessage");
-
-contactForm.addEventListener("submit", async function(event) {
-
-    event.preventDefault();
-
-    const name = document.getElementById("name").value;
+const contactForm = document.getElementById("contactForm"); 
+const successMessage = document.getElementById("successMessage"); 
+contactForm.addEventListener("submit", async function(event) { event.preventDefault(); 
+    const name = document.getElementById("name").value; 
     const email = document.getElementById("email").value;
-    const message = document.getElementById("message").value;
-
-    try {
-
-        const response = await fetch("https://krishna-portfolio-1.onrender.com/api/messages", {
-            method: "POST",
-
-            headers: {
-                "Content-Type": "application/json"
-            },
-
-            body: JSON.stringify({
-                name: name,
-                email: email,
-                message: message
-            })
-        });
-
-        const data = await response.json();
-
-        if (data.success) {
-
-            successMessage.style.display = "block";
-
-            contactForm.reset();
-
-        } else {
-
+    const message = document.getElementById("message").value; 
+    try { const response = await fetch("https://krishna-portfolio-1.onrender.com/api/messages", {
+         method: "POST", headers: { "Content-Type": "application/json" }, 
+         body: JSON.stringify({ name: name, email: email, message: message }) }); 
+         const data = await response.json(); 
+         if (data.success) { 
+            successMessage.style.display = "block"; contactForm.reset();
+         }
+        else { 
             alert(data.message);
-        }
-
-    } catch (error) {
-
-        console.error(error);
-
-        alert("Unable to connect to server.");
-    }
-});
+         } } 
+         catch (error) { console.error(error); 
+            alert("Unable to connect to server."); } });
